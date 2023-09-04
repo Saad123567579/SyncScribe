5fb0aba548d3d3f97195f66a636747cf917e37ed
Muhammad Saad <msaad.bscs22seecs@seecs.edu.pk>
2023-09-05 00:47:26 +0500

    quill setup

--- src/components/Document.jsx
+++ src/components/Document.jsx
@@ -1,28 +1,89 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useRef} from 'react'
 import { useParams } from 'react-router'
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router';
-import { setUser } from '../redux/authSlice';
+import { setUser, setMyDocs } from '../redux/authSlice';
+import "quill/dist/quill.snow.css";
+import Quill from 'quill';
+import { documentRef } from '../firebase';
+import { query, where, getDocs } from 'firebase/firestore';
+import Navbar from './Navbar';
 const Document = () => {
     const { id } = useParams();
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const wrapperRef = useRef();
+    
+    
     useEffect(() => {
-        const doTask = async () => {
-            let user = JSON.parse(localStorage.getItem("userObject"));
-            // if(!user) window.location.href = "/";
-            if (!user) navigate("/");
-            await dispatch(setUser(user));
-        }
-        return () => doTask();
+      const doTask = async () => {
+          let user = JSON.parse(localStorage.getItem("userObject"));
+          // if(!user) window.location.href = "/";
+          if (!user) navigate("/");
+          const firestoreQuery1 = query(documentRef, where("host.name", "==", user.name));
+          const fetchedUsers = await getDocs(firestoreQuery1);
+          const allDocs = [];
+          fetchedUsers.forEach((doc) => {
+              allDocs.push(doc.data());
+          });
+
+          const firestoreQuery2 = query(documentRef, where("allowedUsers", "array-contains", user.name));
+          const fetchedUsersAgain = await getDocs(firestoreQuery2);
+          fetchedUsersAgain.forEach((doc) => {
+              let d = doc.data();
+              let by = d.name;
+              d.sharedBy = by;
+              allDocs.push(d);
+          });
+          // console.log('the docs are',allDocs);
+          await dispatch(setMyDocs(allDocs));
+          await dispatch(setUser(user));
+      }
+      return () => doTask();
 
 
 
-    }, [dispatch, navigate])
+  }, [dispatch, navigate])
+
+  const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ align: [] }],
+    ["image", "blockquote", "code-block"],
+    ["clean"],
+  ]
+
+  useEffect(() => {
+    const editor = document.createElement('div');
+    
+      if(wrapperRef.current.children.length==0){
+        wrapperRef.current.append(editor);
+        new Quill(editor, {
+          theme: "snow",
+          modules: { toolbar: TOOLBAR_OPTIONS },
+        })
+      }
+   
+
+    
+    return () => {
+      wrapperRef.innerHTML = "";
+    }
+  }, [])
+  
+
   return (
-    <div>
-      The id is {id}
-    </div>
+    <>
+    <Navbar />
+    <div className="w-full h-full" id="container" ref={wrapperRef}>
+     
+     </div>
+    </>
+    
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {useSelector} from "react-redux";
+import { useNavigate } from 'react-router';
 
 const Navbar = () => {
+    const navigate = useNavigate();
     const user = useSelector((state)=>state?.auth?.user);
     return (
         <div className='flex flex-wrap items-center justify-between w-full h-16 p-4  bg-gray-100 dark:bg-gray-800'>
             <div className='flex items-center'>
-                <div className='text-4xl text-gray-800 mb-2 dark:text-white mr-2'>
+                <div className='text-4xl text-gray-800 mb-2 dark:text-white mr-2 cursor-pointer' onClick={()=>navigate("/dashboard")}>
                     <svg className='w-10 h-10' aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" strokeWidth="2" d="M3 8v10a1 1 0 0 0 1 1h4v-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v5h4a1 1 0 0 0 1-1V8M1 10l9-9 9 9" />
                     </svg>
@@ -16,7 +18,7 @@ const Navbar = () => {
                         <path stroke="currentColor" strokeWidth="2" d="M4.828 10h6.239m-6.239 4h6.239M6 1v4a1 1 0 0 1-1 1H1m14-4v16a.97.97 0 0 1-.933 1H1.933A.97.97 0 0 1 1 18V5.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 5.828 1h8.239A.97.97 0 0 1 15 2Z" />
                     </svg>
                 </div>
-                <h1 className='font-bold text-4xl mb-2'>Mydoc</h1>
+                <h1 className='font-bold text-xl mb-2 md:text-4xl'>Welcome {user?.name}</h1>
             </div>
             <div className='flex items-center '>
                 <img className='rounded-full w-12 h-12' alt="img" src={user?.image} />
